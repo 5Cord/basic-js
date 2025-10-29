feb10adde0ab5b6645cc51e2772b9c218a2193ef
Vaytovich Dmitriy Valentinovich <102501329+5Cord@users.noreply.github.com>
2025-10-29 09:50:48 +0300

    feat: implement getCommonCharacterCount, countCats, getSeason, deleteDigit, getDNSStats functions

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,18 @@ const { NotImplementedError } = require('../lib');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function deleteDigit(n) {
+  const str = String(n);
+  let max = -Infinity;
+
+  for (let i = 0; i < str.length; i++) {
+    const candidate = Number(str.slice(0, i) + str.slice(i + 1));
+    if (candidate > max) {
+      max = candidate;
+    }
+  }
+
+  return max;
 }
 
 module.exports = {
